9fd6fdf6792785a4913edb21ed0923310d3902ee
Gregorius Ivan Halim <99463068+Grego003@users.noreply.github.com>
2025-09-19 19:14:16 +0700

    fix: change BlazorWASM to server

--- src/AutomotiveApp.BlazorUI/wwwroot/lib/bootstrap/dist/js/bootstrap.bundle.js
+++ src/AutomotiveApp.BlazorUI/wwwroot/lib/bootstrap/dist/js/bootstrap.bundle.js
@@ -1128,7 +1128,7 @@
     [ARROW_RIGHT_KEY$1]: DIRECTION_LEFT
   };
   const Default$b = {
-    interval: 5220,
+    interval: 5000,
     keyboard: true,
     pause: 'hover',
     ride: false,
@@ -6137,7 +6137,7 @@
   const Default = {
     animation: true,
     autohide: true,
-    delay: 5220
+    delay: 5000
   };
 
   /**
