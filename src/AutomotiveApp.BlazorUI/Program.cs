a34e5463871603b76806cbe3592e6171b1269bf5
kiet <kiet123pascal@gmail.com>
2025-09-24 13:53:44 +0700

    feat: added CourseCard section & IDR localization

--- src/AutomotiveApp.BlazorUI/Program.cs
+++ src/AutomotiveApp.BlazorUI/Program.cs
@@ -1,5 +1,6 @@
 using AutomotiveApp.BlazorUI.Components;
 using MudBlazor.Services;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,8 @@ builder.Services.AddRazorComponents()
 //Mud blazor implementation
 builder.Services.AddMudServices();
 
+builder.Services.AddLocalization();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,6 +22,10 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+var cultureInfo = new CultureInfo("id-ID");
+CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
 
 app.UseHttpsRedirection();
 
