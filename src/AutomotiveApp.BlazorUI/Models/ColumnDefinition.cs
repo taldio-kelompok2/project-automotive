f397fed1a2325d0a178db26fd1fed8c488773c52
kiet <kiet123pascal@gmail.com>
2025-10-02 18:24:29 +0700

    feat: added table component and AdminUser page

---
+++ src/AutomotiveApp.BlazorUI/Models/ColumnDefinition.cs
@@ -0,0 +1,11 @@
+﻿namespace AutomotiveApp.BlazorUI.Models
+{
+    public class ColumnDefinition<T>
+    {
+        public string Header { get; set; }
+        public Func<T, object> CellTemplate { get; set; }
+        public bool IsSearchable { get; set; } = false;
+        public bool IsStatusColumn { get; set; }
+        public bool IsActionColumn { get; set; }
+    }
+}
