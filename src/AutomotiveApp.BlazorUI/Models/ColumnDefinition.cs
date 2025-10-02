24de6a42a1bc6970eafc6999da3926c7e80d349f
kiet <kiet123pascal@gmail.com>
2025-10-02 21:16:16 +0700

    feat: added course, invoice, payment method, category admin pages

--- src/AutomotiveApp.BlazorUI/Models/ColumnDefinition.cs
+++ src/AutomotiveApp.BlazorUI/Models/ColumnDefinition.cs
@@ -5,7 +5,11 @@
         public string Header { get; set; }
         public Func<T, object> CellTemplate { get; set; }
         public bool IsSearchable { get; set; } = false;
-        public bool IsStatusColumn { get; set; }
-        public bool IsActionColumn { get; set; }
+        public bool IsStatusColumn { get; set; } = false;
+        public bool IsActionColumn { get; set; } = false;
+        public bool IsImageColumn { get; set; } = false;
+
+        public bool ShowEdit { get; set; } = false;
+        public bool ShowDetails { get; set; } = false;
     }
 }
