b2c284a176230592a9c2c4d893de09d846359b19
Aditya Pratama Febriono <adityapeef18@gmail.com>
2025-10-06 01:35:59 +0700

    Feat: EndPoint OrderItem

--- src/AutomotiveApp.Domain/Entities/Orders/OrderItem.cs
+++ src/AutomotiveApp.Domain/Entities/Orders/OrderItem.cs
@@ -1,18 +1,43 @@
+// using AutomotiveApp.Base.Entities;
+// using AutomotiveApp.Domain.Entities.Courses;
+
+// namespace AutomotiveApp.Domain.Entities.Orders
+// {
+//     public class OrderItem : BaseEntity
+//     {
+//         public required uint Price { get; set; }
+
+//         // Foreign Key
+//         public Guid OrderId { get; set; }
+//         public Guid SessionId { get; set; }
+
+//         // Navigation properties
+//         public virtual Order Order { get; set; } = null!;
+//         public virtual CourseSession Session { get; set; } = null!;
+//     }
+// }
+
 using AutomotiveApp.Base.Entities;
 using AutomotiveApp.Domain.Entities.Courses;
+using AutomotiveApp.Domain.Entities.Orders;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AutomotiveApp.Domain.Entities.Orders
 {
     public class OrderItem : BaseEntity
     {
-        public required uint Price { get; set; }
+        // public required uint Price { get; set; }
+        public required long Price { get; set; }  // bigint -> long
 
-        // Foreign Key
+        // Foreign Keys
         public Guid OrderId { get; set; }
         public Guid SessionId { get; set; }
 
         // Navigation properties
+        [ForeignKey(nameof(OrderId))]
         public virtual Order Order { get; set; } = null!;
+
+        [ForeignKey(nameof(SessionId))]
         public virtual CourseSession Session { get; set; } = null!;
     }
 }
