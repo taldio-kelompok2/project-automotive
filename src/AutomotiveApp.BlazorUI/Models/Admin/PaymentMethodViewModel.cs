919ccda672f76c6434bd2a2b7aadca6ec9aaa05b
Aditya Pratama Febriono <adityapeef18@gmail.com>
2025-10-15 11:01:39 +0700

    Fix: Problems

--- src/AutomotiveApp.BlazorUI/Models/Admin/PaymentMethodViewModel.cs
+++ src/AutomotiveApp.BlazorUI/Models/Admin/PaymentMethodViewModel.cs
@@ -8,12 +8,12 @@ namespace AutomotiveApp.BlazorUI.Models.Admin
     {
         public int ID { get; set; }
         public Guid PaymentMethodId { get; set; }
-        public string Name { get; set; }
+        public string  Name { get; set; } = "";
         public string Logo { get; set; } = ""; 
         public Status Status { get; set; }
 
         public string? ImageFileName { get; set; }
-        // public IFormFile? FileImageName { get; set; } 
+
         public IBrowserFile? File { get; set; }
     }
 }
