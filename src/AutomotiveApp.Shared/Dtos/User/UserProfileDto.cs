c31fbf41a0ce9ece344b0c90f43e37bec9b0dbd9
Gregorius Ivan Halim <99463068+Grego003@users.noreply.github.com>
2025-10-16 18:12:24 +0700

    fix: invoice not showing 2

--- src/AutomotiveApp.Shared/Dtos/User/UserProfileDto.cs
+++ src/AutomotiveApp.Shared/Dtos/User/UserProfileDto.cs
@@ -1,7 +1,7 @@
 ﻿
-using AutomotiveApp.Application.Orders;
 using AutomotiveApp.Shared.Dtos.Carts;
 using AutomotiveApp.Shared.Dtos.Courses;
+using AutomotiveApp.Shared.Dtos.Order;
 
 namespace AutomotiveApp.Shared.Dtos.User
 {
