65583694745c30a5283ee2260bcb733db0f19e17
Aditya Pratama Febriono <adityapeef18@gmail.com>
2025-10-16 14:21:19 +0700

    Fix: update namespace order DTO reference

--- src/AutomotiveApp.Shared/Dtos/User/UserProfileDto.cs
+++ src/AutomotiveApp.Shared/Dtos/User/UserProfileDto.cs
@@ -1,5 +1,6 @@
 ﻿
-using AutomotiveApp.Application.Orders;
+// using AutomotiveApp.Application.Orders;
+using AutomotiveApp.Shared.Dtos.Order;
 using AutomotiveApp.Shared.Dtos.Carts;
 using AutomotiveApp.Shared.Dtos.Courses;
 
