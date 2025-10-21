35716d79299537a70a4e35d778bb4fc3bef7471e
Aditya Pratama Febriono <adityapeef18@gmail.com>
2025-10-21 23:20:22 +0700

    Feat: Add Description at Admin Course Management

--- src/AutomotiveApp.WebAPI/Dto/Courses/CourseEditRequest.cs
+++ src/AutomotiveApp.WebAPI/Dto/Courses/CourseEditRequest.cs
@@ -6,6 +6,7 @@ namespace AutomotiveApp.WebAPI.Dto.Courses
     public class CourseEditRequest : BaseCommandDto, ICommandDto
     {
         public string? Name { get; set; }
+        public string? Description { get; set; }
         public int? Price { get; set; }
         public Guid? CategoryId { get; set; }
         public IFormFile? Image { get; set; }
